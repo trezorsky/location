ad8c1a7740178fe070f316bd682a7fc0af8d322c
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-11-11 13:11:25 +0300

    module5_q22

--- src/main/java/com/example/location/controller/WeatherController.java
+++ src/main/java/com/example/location/controller/WeatherController.java
@@ -4,6 +4,8 @@ import com.example.location.model.Geodata;
 import com.example.location.model.Weather;
 import com.example.location.repository.GeodataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,13 +17,18 @@ public class WeatherController {
     @Autowired
     private GeodataRepository repository;
     @Autowired
-    RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate;
 
     @GetMapping("/weather")
-    public Weather redirectRequestWeather(@RequestParam String location) {
-        Geodata geodata = repository.findByName(location).get();
-        String url = String.format("http://localhost:8082/?lat=%s&lon=%s", geodata.getLat(), geodata.getLon());
-        return restTemplate.getForObject(url, Weather.class);
+    public ResponseEntity<Weather> redirectRequestWeather(@RequestParam String location) {
+        Optional<Geodata> optionalGeodata = repository.findByName(location);
+        if (optionalGeodata.isPresent()) {
+            Geodata geodata = optionalGeodata.get();
+            String url = String.format("http://localhost:8082/?lat=%s&lon=%s", geodata.getLat(), geodata.getLon());
+            return new ResponseEntity(restTemplate.getForObject(url, Weather.class), HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping
